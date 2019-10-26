0246ba140d96d6350b11a96c1bc985bb8a177207
JohnWarriorCor <guerrerocordobajohn@gmail.com>
2019-10-26 09:02:06 -0500

    Nuevas mejoras y nuevas peliculas 26/10/2019 9:01 am

--- src/app/components/tarjeta-peliculas/tarjeta-peliculas.component.ts
+++ src/app/components/tarjeta-peliculas/tarjeta-peliculas.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tarjeta-peliculas',
   templateUrl: './tarjeta-peliculas.component.html',
-  styleUrls: ['./tarjeta-peliculas.component.css']
+  styleUrls: ['./tarjeta-peliculas.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TarjetaPeliculasComponent {
   @Input() pelicula: any = {};
--- src/app/services/pelicula-bd.service.ts
+++ src/app/services/pelicula-bd.service.ts
@@ -8,7 +8,8 @@ export class PeliculaBdService {
   private peliculas: Peliculas[] = [
     {
       idx : 0,
-      titulo: 'La vida secreta de tus mascotas 2',
+      titulo: 'PETS 2',
+      short: 'LA VIDA SECRETA DE TUS MASCOTAS',
       // tslint:disable-next-line:max-line-length
       sinopsis: 'Max enfrenta algunos cambios importantes después de que su dueña Katie se case y ahora tenga un hijo. En un viaje familiar al campo, Max conoce a un perro de granja llamado Rooster, y ambos intentan superar sus miedos. Mientras tanto, Gidget intenta rescatar el juguete favorito de Max de un apartamento lleno de gatos, y Snowball se embarca en una misión para liberar a un tigre blanco llamado Hu de un circo.',
       img: 'assets/pets2.jpg',
@@ -18,7 +19,8 @@ export class PeliculaBdService {
     },
     {
       idx : 1,
-      titulo: 'Fast & Furious: Hobbs & Shaw',
+      titulo: 'Hobbs & Shaw',
+      short: 'Fast & Furious: Hobbs & Shaw',
       // tslint:disable-next-line:max-line-length
       sinopsis: 'El abogado Luke Hobbs y el marginado Deckard Shaw forman una alianza improbable cuando un villano cibernéticamente mejorado amenaza el futuro de la humanidad.',
       img: 'assets/FYF.jpg',
@@ -28,13 +30,47 @@ export class PeliculaBdService {
     },
     {
       idx : 2,
-      titulo: 'El rey león - The Lion King',
+      titulo: 'El rey león',
+      short: 'El rey león',
       // tslint:disable-next-line:max-line-length
       sinopsis: 'Después del asesinato de su padre, un joven príncipe león huye de su reino solo para aprender el verdadero significado de responsabilidad y valentía.',
       img: 'assets/rl.jpg',
       fecha: '2019-07-19',
-      trailer: '2ufiKxMAJB8',
+      trailer: '0U-kFH-ixV0',
       drive: '1VKoP_GHb2RqGxKRuiZfoPK33HIHAYJkz/preview'
+    },
+    {
+      idx : 3,
+      titulo: 'Toy Story 4',
+      short: 'Toy Story 4',
+      // tslint:disable-next-line:max-line-length
+      sinopsis: 'Woody, Buzz Lightyear y el resto de la pandilla se embarcan en un viaje por carretera con Bonnie y un nuevo juguete llamado Forky. El viaje aventurero se convierte en una reunión inesperada cuando el pequeño desvío de Woody lo lleva a su amigo Bo Peep, perdido hace mucho tiempo. Mientras Woody y Bo discuten los viejos tiempos, pronto comienzan a darse cuenta de que están separados por dos mundos cuando se trata de lo que quieren de la vida como un juguete.',
+      img: 'assets/toy4.jpg',
+      fecha: '2019-06-21',
+      trailer: '25qmgvuQcSA',
+      drive: '1FdkIicO8Sp8kFkH0sqZN1R2rYjNqxx_d/preview'
+    },
+    {
+      idx : 4,
+      titulo: 'Dectetive Pikachu',
+      short: 'Pokémon: Detective Pikachu',
+      // tslint:disable-next-line:max-line-length
+      sinopsis: 'La historia comienza cuando el detective Harry Goodman desaparece misteriosamente, lo que lleva a su hijo Tim de 21 años a averiguar qué sucedió. Ayudando en la investigación está el antiguo compañero Pokémon de Harry, el Detective Pikachu: un súper detective adorable, hilarante y sabio que es un misterio incluso para sí mismo. Al descubrir que están equipados de manera única para comunicarse entre sí, Tim y Pikachu unen fuerzas en una emocionante aventura para desentrañar el misterio enredado. Persiguiendo pistas por las calles iluminadas con neón de Ryme City, una metrópolis moderna y extensa donde los humanos y los Pokémon viven codo con codo en un mundo hiperrealista de acción en vivo, se encuentran con un elenco diverso de personajes Pokémon y descubren un impactante trama que podría destruir esta coexistencia pacífica y amenazar a todo el universo Pokémon.',
+      img: 'assets/pik.jpg',
+      fecha: '2019-04-10',
+      trailer: 'JralaTqAeDU',
+      drive: '1awgJctSr_9EhrHHqrl7Rx7HS6DefR3Yy/preview'
+    },
+    {
+      idx : 5,
+      titulo: 'Dumbo',
+      short: 'Dumbo',
+      // tslint:disable-next-line:max-line-length
+      sinopsis: 'Holt fue una vez una estrella de circo, pero se fue a la guerra y cuando regresó, lo había alterado terriblemente. El dueño del circo Max Medici (Danny DeVito) lo contrata para cuidar a Dumbo, un elefante recién nacido cuyas orejas de gran tamaño lo convierten en el hazmerreír de la luchadora compañía de circo. Pero cuando los niños de Holt descubren que Dumbo puede volar, el empresario de lengua plateada VA Vandevere (Michael Keaton) y la artista aérea Colette Marchant (Eva Green) se precipitan para convertir al pequeño elefante en una estrella.',
+      img: 'assets/dumbo.jpg',
+      fecha: '2019-03-29',
+      trailer: 'N1S6vO_DbF8',
+      drive: '1asJIBWVocgwP-0JnMZfDh1t3r835mPTu/preview'
     },
   ];
   getPeliculas(): Peliculas[] {
@@ -47,6 +83,7 @@ export class PeliculaBdService {
 }
 export interface Peliculas {
   idx?: number;
+  short: string;
   titulo: string;
   sinopsis: string;
   img: string;
